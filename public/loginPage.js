2c703c381dc189c5b6d27bc97c56f4a47d8d1f0a
“betiwe” <betiwe@yandex.ru>
2024-03-02 11:41:09 +0300

    updated homePage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,27 +1,25 @@
-"use strict"
-
+'use strict';
 
 const userForm = new UserForm();
 
+userForm.loginFormCallback = (data) => {
+  ApiConnector.login(data, (response) => {
+    if (response.success) {
+      location.reload();
+      userForm.setLoginErrorMessage('Вход выполнен успешно');
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+    }
+  });
+};
 
-userForm.loginFormCallback = data => {
-	ApiConnector.login(data, response => {
-		if (response.success) {
-			location.reload();
-			console.log('Вход выполнен успешно');
-		} else { alert(response.error);
-		}
-	})
-}
-
-userForm.registerFormCallback = data => {
-	ApiConnector.login(data, response => {
-		if (response.success) {
-			location.reload();
-			console.log('Регистрация прошла успешно');
-		} else { alert(response.error);
-		}
-	})	
-}
-
-
+userForm.registerFormCallback = (data) => {
+  ApiConnector.login(data, (response) => {
+    if (response.success) {
+      location.reload();
+      userForm.setLoginErrorMessage('Регистрация прошла успешно');
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+    }
+  });
+};
